9193f1da6ce951c7d7255aa5c3e6c8e64298ed8e
JulianTurco17 <36230583+julianturco18@users.noreply.github.com>
2018-02-21 12:29:33 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,10 +3,20 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="si";
+	var promedio;
+	var numero;
 
+	while(respuesta == "si")
+	{
+		numero = parseInt(prompt("Ingrese los digitos"));
+		acumulador = acumulador + numero;
+		contador = contador + 1;
+		respuesta = prompt("desea continuar?");
+	}
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	promedio = acumulador / contador;
+document.getElementById("suma").value=acumulador;
+document.getElementById("promedio").value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,31 @@
-function Mostrar()
-{
+function Mostrar() {
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
-	
-	var respuesta='si';
+	var contador = 0;
+	var positivo = 0;
+	var negativo = 1;
+	var respuesta = 's';
+	var numero;
 
+	do{
+		numero = parseInt(prompt("Ingrese numeros"));
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+	if (numero >= 0)
+	{
+		positivo = positivo + numero;
+	}
+	else
+	{
+		negativo = negativo * numero;
+		contadorNeg++;
+	}
+	respuesta = prompt("Desea continuar?");
+	}while(respuesta == 's');
+
+	document.getElementById('suma').value = positivo;
+	if (contadorNeg==0)
+	{
+		negativo = 0;
+	}
+	document.getElementById('producto').value = negativo;
 
 }//FIN DE LA FUNCIÓN
