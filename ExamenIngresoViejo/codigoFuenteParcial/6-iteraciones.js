77992ed0f1e50e239a2f5ef20e6f1d0e088c9076
JulianTurco17 <36230583+julianturco18@users.noreply.github.com>
2018-03-01 12:22:26 -0300

    Add files via upload

--- ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/6-iteraciones.js
@@ -12,8 +12,18 @@ function Mostrar()
 
     while(venta <= 0)
     {
-        venta = parseFloat(prompt("Error. El importe de venta debe ser mayor a 0. Reingrese"))
+        venta = parseFloat(prompt("Error. El importe de venta debe ser mayor a 0. Reingrese"));+
+    }
+    if(venta > ventaMayor || flag == 0)
+    {
+        ventaMayor = venta;
+    }
+    if(venta < ventaMenor || flag == 0)
+    {
+        ventaMenor = venta;
+        flag = 1;
     }
 }     
+alert("La mayor venta fue $ " + ventaMayor + "y la menor venta $ " + ventaMenor);
 }
 
--- ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
+++ ExamenIngresoViejo/codigoFuenteParcial/7-iteraciones.js
@@ -1,20 +1,34 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
-function Mostrar()
-{
-	var notas;
+function Mostrar() {
+    var nota;
     var sexo;
-    var alumnos = 6;
+    var acumNotas = 0;
     var promedio;
-    var acumulador;
+    var notaBaja;
+    var varonesMas6 = 0;
+    var flag = 0;
 
-    notas = parseInt("ingrese la calificacion: ");
-    sexo = parseInt("ingrese f o m: ");
+    for (i = 0; i < 6; i++) {
+        nota = parseInt(prompt("Ingrese nota: "));
+        while (nota < 0 || nota > 10) {
+            nota = parseInt(prompt("Error. La nota debe estar entre 0 y 10. Reingrese: "));
+        }
+        sexo = prompt("Ingrese sexo: ");
 
-    while(sexo == m && notas >= 6 )
-    {
-
+        while (sexo != 'f' && sexo != 'm') {
+            sexo = prompt("Error. El sexo debe ser f o m. Reingrese sexo: ");
+        }
+        acumNotas += nota;
+        if (nota < notaBaja || flag == 0) {
+            notaBaja = nota;
+            flag = 1;
+        }
+        if (sexo == 'm' && nota >= 6) {
+            varonesMas6++;
+        }
     }
+    promedio = acumNotas / 6;
 
-
+    alert("Promedio de notas: " + promedio + "\nNota mas baja: " + notaBaja + "\nVarones con nota >=6: " + varonesMas6);
 }
 
